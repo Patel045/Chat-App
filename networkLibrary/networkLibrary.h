ad0bc441251251fb1a12350e5395fd4c79c3878b
Patel045 <aayushpatel2904@gmail.com>
2024-08-18 02:28:48 +0530

    Doxygen Comments

--- networkLibrary/networkLibrary.h
+++ networkLibrary/networkLibrary.h
@@ -10,73 +10,209 @@
 #include <boost/asio.hpp>
 #include <boost/lexical_cast.hpp>
 
+/**
+ * @brief The main namespace for the network library.
+ */
+
 namespace networkLibrary
 {
+    /**
+     * @brief Represents a chat session between a server and a client.
+     * 
+     * To be used only on Server. Any reading is done on the Chat Session, 
+     * which then translates it to the server class.
+     * 
+     * The chat session does not implement write operation.
+     */
     class chatSession;
+
+    /**
+     * @brief Namespace containing server-related classes.
+     */
     namespace Server
     {
+        /**
+         * @brief An Asynchronous Server class for handling multiple chat sessions.
+         * 
+         * This Asynchronous Server accepts the message from read operation of chat session,
+         * and is responsible to implement logic and send message to whichsoever client. 
+         */
         class asyncServer;
     };
+
+    /**
+     * @brief Namespace containing client-related classes.
+     */
     namespace Client
     {
+        /**
+         * @brief An asynchronous client class for connecting to the server.
+         */
         class asyncClient;
     };
 };
 
+/**
+ * @brief Asynchronous server class for handling chat sessions and sending message.
+ */
 class networkLibrary::Server::asyncServer
 {
 private:
-    unsigned int m_port;
-    boost::asio::io_context &m_io_context;
-    boost::asio::ip::tcp::acceptor m_acceptor;
+    unsigned int m_port; ///< Port number the server listens on.
+    boost::asio::io_context &m_io_context; ///< Reference to the IO context.
+    boost::asio::ip::tcp::acceptor m_acceptor; ///< Acceptor object for accepting new connections.
+    std::set<std::shared_ptr<chatSession>> m_chat_sessions; ///< Set of active chat sessions.
+    std::mutex _server_mutex; ///< Mutex for thread-safe operations on the set of chat essions.
+
+    /**
+     * @brief Starts accepting new chat sessions.
+     */
     void startAccept();
-    std::set<std::shared_ptr<chatSession>> m_chat_sessions;
-    std::pair<std::string,std::string> parse(std::string&);
-    std::mutex _server_mutex;
-    void add_session(const std::shared_ptr<networkLibrary::chatSession>);
-    void remove_session(const std::shared_ptr<networkLibrary::chatSession>);
+
+    /**
+     * @brief Parses a message into a pair of strings.
+     * @param[in,out] message The message to parse.
+     * @return A pair containing the parsed data.
+     */
+    std::pair<std::string, std::string> parse(std::string &message);
+
+    /**
+     * @brief Adds a new chat session to the server.
+     * @param session Shared Pointer to the chat session to add.
+     */
+    void add_session(const std::shared_ptr<networkLibrary::chatSession> session);
+
+    /**
+     * @brief Removes a chat session from the server.
+     * @param session Shared Pointer to the chat session to remove.
+     */
+    void remove_session(const std::shared_ptr<networkLibrary::chatSession> session);
+
 public:
+
     friend class networkLibrary::chatSession;
-    asyncServer(boost::asio::io_context &, unsigned int);
-    void write_broadcast(std::string);
-    void write(const std::shared_ptr<networkLibrary::chatSession>, std::string);
-    void write(const std::shared_ptr<networkLibrary::chatSession>, boost::asio::mutable_buffers_1);
+
+    /**
+     * @brief Constructs an asynchronous server.
+     * @param io_context The IO context used for asynchronous operations.
+     * @param port The port number on which the server will listen.
+     */
+    asyncServer(boost::asio::io_context &io_context, unsigned int port);
+
+    /**
+     * @brief Broadcasts a message to all connected chat sessions.
+     * @param message The message to broadcast.
+     */
+    void write_broadcast(std::string message);
+
+    /**
+     * @brief Sends a message to a specific chat session.
+     * @param session Shared Pointer to the chat session to send the message to.
+     * @param message The message to send.
+     */
+    void write(const std::shared_ptr<networkLibrary::chatSession> session, std::string message);
+
+    /**
+     * @brief Sends a buffer to a specific chat session.
+     * @param session Shared Pointer to the chat session to send the buffer to.
+     * @param buffer The buffer to send.
+     */
+    void write(const std::shared_ptr<networkLibrary::chatSession> session, boost::asio::mutable_buffers_1 buffer);
 };
 
+/**
+ * @brief Asynchronous client class for connecting to the chat server.
+ */
 class networkLibrary::Client::asyncClient
 {
-private:    
-    boost::asio::io_context &m_io_context;
-    std::string m_ip;
-    unsigned int m_port;
-    boost::asio::ip::tcp::socket m_socket;
-    boost::asio::ip::tcp::resolver m_resolver;
-    std::string m_buffer;
+private:
+    boost::asio::io_context &m_io_context; ///< Reference to the IO context.
+    std::string m_ip; ///< Server IP address.
+    unsigned int m_port; ///< Server port number.
+    boost::asio::ip::tcp::socket m_socket; ///< Socket for communication with the server.
+    boost::asio::ip::tcp::resolver m_resolver; ///< Resolver for determining server address.
+    std::string m_buffer; ///< Buffer for storing received data.
+
+    /**
+     * @brief Continuously reads data from the server.
+     */
     void read_continous();
+
 public:
-    std::string m_username;
-    asyncClient(boost::asio::io_context&, std::string, unsigned int);
-    bool write(std::string&);
-    bool write_now(std::string);
+    std::string m_username; ///< Username of the client.
+
+    /**
+     * @brief Constructs an asynchronous client.
+     * @param io_context The IO context used for asynchronous operations.
+     * @param ip The IP address of the server.
+     * @param port The port number of the server.
+     */
+    asyncClient(boost::asio::io_context &io_context, std::string ip, unsigned int port);
+
+    /**
+     * @brief Sends a message to the server.
+     * @param message The message to send.
+     * @return True if the message was successfully sent, false otherwise.
+     */
+    bool write(std::string &message);
+
+    /**
+     * @brief Immediately sends a message to the server.
+     * @param message The message to send.
+     * @return True if the message was successfully sent, false otherwise.
+     */
+    bool write_now(std::string message);
+
+    /**
+     * @brief Closes the connection to the server.
+     */
     void close_connection();
+
+    /**
+     * @brief Destructor for the asynchronous client.
+     */
     ~asyncClient();
 };
 
+/**
+ * @brief Represents a chat session between a server and a client.
+ */
 class networkLibrary::chatSession : public std::enable_shared_from_this<networkLibrary::chatSession>
 {
 private:
-    networkLibrary::Server::asyncServer &m_serv;
-    std::string m_buffer;
-    boost::asio::ip::tcp::socket m_socket;
-    std::string m_ip;
-    unsigned int m_port;
+    networkLibrary::Server::asyncServer &m_serv; ///< Reference to the associated server.
+    std::string m_buffer; ///< Buffer for storing received data.
+    boost::asio::ip::tcp::socket m_socket; ///< Socket for communication.
+    std::string m_ip; ///< Client's IP address.
+    unsigned int m_port; ///< Client's port number.
+
 public:
+
     friend class networkLibrary::Server::asyncServer;
-    chatSession(boost::asio::ip::tcp::socket, networkLibrary::Server::asyncServer&);
+
+    std::string m_name; ///< Name of the chat participant.
+
+    /**
+     * @brief Constructs a chat session.
+     * @param socket The socket used for the chat session.
+     * @param serv The server managing the session.
+     */
+    chatSession(boost::asio::ip::tcp::socket socket, networkLibrary::Server::asyncServer &serv);
+
+    /**
+     * @brief Destructor for the chat session.
+     */
     ~chatSession();
+
+    /**
+     * @brief Starts the chat session.
+     */
     void start();
+
+    /**
+     * @brief Continuously reads data from the client.
+     */
     void read_continous();
-    std::string m_name;
 };
 
 #endif
--- utils/Logger.h
+++ utils/Logger.h
@@ -6,29 +6,85 @@
 #include <string>    // for dealing with names
 #include <ctime>     // for writing time 
 
-class Logger{
+/**
+ * @brief A class for logging messages to different locations such as text files or databases.
+ *
+ * The Logger class provides functionality to log messages to various locations depending on the configuration.
+ */
+
+class Logger {
 public:
-    enum Location{
-        DISABLED,
-        TEXT_FILE,
-        DATABASE
+    /**
+     * @brief Enum representing the possible logging locations.
+     */
+    enum Location {
+        DISABLED,   ///< Logging is disabled.
+        TEXT_FILE,  ///< Log messages to a text file.
+        DATABASE    ///< Log messages to a database.
     };
+
 private:
-    const std::string m_FileName;   //do we need static?
-    std::ofstream m_OutStream;
-    const Location m_Location;
+    const std::string m_FileName;   ///< The name of the file where logs will be written.
+    std::ofstream m_OutStream;      ///< Output stream for writing to the log file.
+    const Location m_Location;      ///< The location where logs should be written.
+
+    /**
+     * @brief Initializes the logger based on the specified location.
+     * @return True if initialization was successful, false otherwise.
+     */
     bool init();
+
 protected:
-    std::string m_ClassName;  //inherit to children
+    std::string m_ClassName;  ///< The name of the class, intended for use by derived classes.
+
 public:
+    /**
+     * @brief Default constructor that initializes the logger with logging disabled.
+     */
     Logger();
-    Logger(const Location&);
-    Logger(const std::string&, const Location&);
-    Logger(const Logger&) = delete;
+
+    /**
+     * @brief Constructor that initializes the logger with the specified logging location.
+     * @param loc The location where logs should be written.
+     */
+    Logger(const Location &loc);
+
+    /**
+     * @brief Constructor that initializes the logger with a file name and logging location.
+     * @param fileName The name of the file where logs will be written.
+     * @param loc The location where logs should be written.
+     */
+    Logger(const std::string &fileName, const Location &loc);
+
+    /**
+     * @brief Copy constructor is deleted to prevent copying of the logger.
+     */
+    Logger(const Logger &) = delete;
+
+    /**
+     * @brief Destructor that cleans up resources used by the logger.
+     */
     ~Logger();
+
+    /**
+     * @brief Logs an empty string.
+     * @return True if the log was successful, false otherwise.
+     */
     bool log();
-    bool log(const std::string&);
-    bool log(const std::initializer_list<std::string>&);
+
+    /**
+     * @brief Logs a specific message.
+     * @param message The message to be logged.
+     * @return True if the log was successful, false otherwise.
+     */
+    bool log(const std::string &message);
+
+    /**
+     * @brief Logs multiple messages provided in an initializer list.
+     * @param messages The list of messages to be logged.
+     * @return True if the log was successful, false otherwise.
+     */
+    bool log(const std::initializer_list<std::string> &messages);
 };
 
-#endif
+#endif // LOGGER_H
