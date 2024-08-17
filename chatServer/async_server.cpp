10d66949d6bd06a6f103d6effa2a2b564a031ce0
Patel045 <aayushpatel2904@gmail.com>
2024-08-17 15:30:39 +0530

    Implemented Multithreading

--- chatServer/async_server.cpp
+++ chatServer/async_server.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <thread>
 #include <boost/asio.hpp>
 
 #include "../networkLibrary/networkLibrary.h"
@@ -19,7 +20,21 @@ int main(int argc, char* argv[]){
     
     boost::asio::io_context io_context;
     networkLibrary::Server::asyncServer server(io_context, port);
-    io_context.run();
+
+    std::cout << "Threads Available " << std::thread::hardware_concurrency() << std::endl;
+
+    std::vector<std::thread> all_threads;
+    for(std::size_t i=0; i<std::thread::hardware_concurrency(); ++i){
+        all_threads.emplace_back(
+            [&io_context](){
+                io_context.run();
+            }
+        );
+    }
+
+    for(auto& my_thread : all_threads){
+        my_thread.join();
+    }
 
     return 0;
 }
